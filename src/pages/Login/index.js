bfcd45ead1dab7df8c2af97e9e64ba2e77b9f74a
leepoo <leepoo2008@163.com>
2020-03-29 00:40:08 +0800

    编写登录页

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,11 +1,61 @@
 import React, { Component } from 'react';
 import './index.scss';
+import { Form, Input, Button, Checkbox } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+
+
 class Login extends Component {
+
+   onFinish = values => {
+    console.log('Received values of form: ', values);
+  };
+  
 
   render() {
     return(
       <div className="container">
-        
+        <div className="login-form">
+        <Form
+          name="normal_login"
+          className="login-form"
+          initialValues={{ remember: true }}
+          onFinish={this.onFinish}
+        >
+          <div className="title">品连后台管理系统</div>
+          <Form.Item
+            name="username"
+            rules={[{ required: true, message: 'Please input your Username!' }]}
+          >
+            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: 'Please input your Password!' }]}
+          >
+            <Input.Password
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+
+            <a className="login-form-forgot" href="">
+              Forgot password
+            </a>
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="login-form-button">
+              Log in
+            </Button>
+            Or <a href="">register now!</a>
+          </Form.Item>
+        </Form>
+        </div>
       </div>
     )
   }
